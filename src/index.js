c916f10909b9861d32e379e81a04efcf25992a81
AllexBlaze <naum4ick@yandex.ru>
2018-09-28 04:04:02 +0300

    Factorial Zeros Calculation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let ourNumber = number, zCounter = 0;
+  while( ourNumber > 0){
+    ourNumber = Math.floor(ourNumber/5);
+    zCounter += ourNumber;
+  }
+  return zCounter;
 }
