2e93f03971a47e2e5a1a244abd955f4c994d0203
AllexBlaze <naum4ick@yandex.ru>
2018-09-29 22:04:52 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number) {
   let ourNumber = number, zCounter = 0;
   while( ourNumber > 0){
-    ourNumber = Math.floor(ourNumber/5);
+    ourNumber = ~~ourNumber/5;
     zCounter += ourNumber;
   };
   return zCounter;
